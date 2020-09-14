38eb781069dd44768ffd8a04280056543453f2ac
Stephen Nicholas <stephennicholas.191200@gmail.com>
2020-09-14 14:06:01 +0700

    Added Action Dial Feature in BottomSheets

--- app/src/main/java/com/example/bnccacademy/HotlineViewHolder.kt
+++ app/src/main/java/com/example/bnccacademy/HotlineViewHolder.kt
@@ -1,10 +1,11 @@
 package com.example.bnccacademy
 
+import android.content.Intent
+import android.net.Uri
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_hotline.view.*
-import kotlinx.android.synthetic.main.item_look_up.view.tvLookUpProvinceName
 
 class HotlineViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     fun bind(data: HotlineData) {
@@ -14,5 +15,19 @@ class HotlineViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
             Picasso.get().load(data.imgIcon).into(itemView.image)
         }
         // picasso akan meload data dari gambar yang berdasarkan link tersebut
+    }
+
+    init {
+        itemView.setOnClickListener{
+            actionDial(itemView, this.itemView.tvHotLineNumber.text)
+        }
+    }
+
+    private fun actionDial(itemView: View, phoneNumber: CharSequence) {
+        val intent = Intent().apply {
+            action = Intent.ACTION_DIAL
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        itemView.context.startActivity(intent)
     }
 }
